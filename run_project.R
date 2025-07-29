219e7ab683b190d66f6b30b6ad3a05228d796ade
Ken Murayama <223380853+Squarecat8@users.noreply.github.com>
2025-07-28 21:24:27 -0400

    Adds report.Rmd and run_project.R

---
+++ run_project.R
@@ -0,0 +1,24 @@
+# run_project.R
+# This script automate the report production
+
+# Run `renv::restore()` to install all the necessary packages
+# 
+library(here)
+
+# Check the data files
+check_data <- function(file_name, file_dir = "01_rawdata"){
+  # Check the existence of data files before running scripts
+  if (file.exists(here(file_dir, file_name))) {
+    message(paste("The data file", file_name, "exist."))
+  } else {
+    message(paste("The data file", file_name, "does not exist. Check the sources."))
+  }
+}
+
+# Confirm that the data files exist
+check_data("On-track and off-track countries.xlsx")
+check_data("unicef-data-20250727.csv")
+check_data("WPP2022_GEN_F01_DEMOGRAPHIC_INDICATORS_COMPACT_REV1.xlsx")
+
+# Compile the report 
+rmarkdown::render("your_document.Rmd")
